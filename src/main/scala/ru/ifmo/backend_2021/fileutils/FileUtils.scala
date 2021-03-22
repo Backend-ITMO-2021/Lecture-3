eff92414b946d555637f090df6002e91c39e9ed6
SpaceOcean <komsomolenko11@gmail.com>
2021-03-22 11:48:59 +0300

    fix

--- src/main/scala/ru/ifmo/backend_2021/fileutils/FileUtils.scala
+++ src/main/scala/ru/ifmo/backend_2021/fileutils/FileUtils.scala
@@ -5,14 +5,14 @@ import java.io.{BufferedReader, BufferedWriter, FileReader, PrintWriter}
 
 class FileUtils {
   def withFileReader(nameFile: String)(read: BufferedReader => String) = {
-    var file = new BufferedReader(new FileReader(nameFile))
-    var out = new StringBuilder(read(file))
+    val file = new BufferedReader(new FileReader(nameFile))
+    val out = new StringBuilder(read(file))
     file.close()
     out.toString()
   }
 
   def withFileWriter(nameFile: String)(write: BufferedWriter => Unit) = {
-    var file = new BufferedWriter(new PrintWriter(nameFile))
+    val file = new BufferedWriter(new PrintWriter(nameFile))
     write(file)
     file.close()
   }
